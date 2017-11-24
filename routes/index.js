a421d80d08c0f3dcdb9ffd204b3749e75e46fd66
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-24 23:14:14 +0300

    get api key,  render errot page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Прогноз погоды в городе' });
+});
+
+router.post('/', (req, res, next) => {
+  const city = req.body.city;
+  const url = `api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${process.env.API_KEY}`
+
+  request(url, (error, response, body)=>{
+    if(error){
+      res.render('error', {
+        message: 'Error',
+        error
+      })
+    }
+  })
 });
 
 module.exports = router;
